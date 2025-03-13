d0e3f45b38302298fa970d887b28a3c6873ba977
Ng Chuan Seng <waelectriz@gmail.com>
2025-03-13 20:32:07 +0800

    [Refactor] Move setup_config to own directory

--- main.py
+++ main.py
@@ -1,34 +1,15 @@
 from flask import Flask, render_template, request, jsonify
 from datetime import datetime
 
+import setup.setup_stg as setup_stg
+
 import requests
 import sqlite3
-import yaml
 
 app = Flask(__name__)
 
 
-# Function to map yaml file's user-config to parameters
-def cfg_setup():
-    # Load config file
-    with open("cfg/user_config.yaml", "r") as f:
-        config = yaml.safe_load(f)
-
-    # Accessing config settings
-    api_key = config["api_key"]
-
-    # Make sure API key exists before continuing script
-    if not (api_key):
-        print(
-            "Create personal API key at exchangerate-api.com and input to user_config.yaml"
-        )
-        print("Do not upload personal API key!")
-        raise ValueError("API Key is empty in user_config.yaml!")
-    else:
-        return api_key
-
-
-api_key = cfg_setup()
+api_key = setup_stg.cfg_setup()
 
 API_URL = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/"
 
